13ab9c8b703d3390bb0123288c75bbc15dac1538
Hasan Can <hasancankucuk@yandex.com>
2025-06-26 13:48:00 +0200

    update

--- backend/scripts/doppler_env.py
+++ backend/scripts/doppler_env.py
@@ -1,8 +1,10 @@
+import os
 from dopplersdk import DopplerSDK
 
 def generate_env_file(access_token: str, project: str, config: str, output_path: str = ".env"):
     doppler = DopplerSDK()
     doppler.set_access_token(access_token)
+    print("projcts", project)
     print(f"Fetching secrets for project '{project}' / config '{config}'...")
 
     try:
@@ -20,9 +22,13 @@ def generate_env_file(access_token: str, project: str, config: str, output_path:
 
 
 def main():
-    ACCESS_TOKEN = "dp.st.dev.mvZ7h5bvWTYOKN0WcvJqdoGXqhAgQLJhEtacYkQ687d"
+    ACCESS_TOKEN = os.getenv("DOPPLER_TOKEN")
     PROJECT = "order-tracking-bot"
     CONFIG = "dev"
+    
+    print(f"🔑 Using Doppler Token: {ACCESS_TOKEN[:20]}...")
+    print(f"📁 Project: {PROJECT}")
+    print(f"⚙️  Config: {CONFIG}")
 
     generate_env_file(ACCESS_TOKEN, PROJECT, CONFIG)
 
